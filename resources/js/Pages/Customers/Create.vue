98a94cc3f988def753f738235efd341ba0c220a7
yuta higa <h.yuta2314@gmail.com>
2023-02-12 14:47:46 +0900

    Added YubinBangoCore.

--- resources/js/Pages/Customers/Create.vue
+++ resources/js/Pages/Customers/Create.vue
@@ -4,6 +4,7 @@ import InputError from '@/Components/InputError.vue';
 import { Head } from '@inertiajs/vue3';
 import { reactive } from 'vue';
 import { Inertia } from '@inertiajs/inertia';
+import { Core as YubinBangoCore } from "yubinbango-core2";
 
 defineProps({
   errors: Object
@@ -23,6 +24,13 @@ const form = reactive({
 
 const storeCustomer = () => {
   Inertia.post('/customers', form)
+}
+
+const fetchAddress = () => {
+  new YubinBangoCore(String(form.postcode), (value) => {
+    // console.log(value)
+    form.address = value.region + value.locality + value.street
+  })
 }
 </script>
 
@@ -81,7 +89,7 @@ const storeCustomer = () => {
                             <div class="p-2 w-full">
                               <div class="relative">
                                 <label for="postcode" class="leading-7 text-sm text-gray-600">郵便番号</label>
-                                <input type="number" id="postcode" name="postcode" v-model="form.postcode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                <input type="number" id="postcode" name="postcode" @change="fetchAddress" v-model="form.postcode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                               </div>
                               <InputError class="mt-2" :message="errors.postcode" />
                             </div>
