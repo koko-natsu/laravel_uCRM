2ee9a5746f9b9842d1fa5c8af3652b3540be1dfb
yuta higa <h.yuta2314@gmail.com>
2023-02-13 18:02:50 +0900

    Migrated ItemPurchaseTable and created sample datas.

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Customer;
+use App\Models\Item;
+
 
 class Purchase extends Model
 {
@@ -15,7 +17,13 @@ class Purchase extends Model
         'status',
     ];
 
+    /* Relation */
     public function customer() {
         return $this->belongsTo(Customer::class);
+    }
+
+    public function items() {
+        return $this->belongsToMany(item::class)
+        ->withPivot('quantity');
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Customer;
+use App\Models\Purchase;
+use App\Models\Item;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,7 +22,22 @@ class DatabaseSeeder extends Seeder
             ItemSeeder::class,
         ]);
 
-        \App\Models\Customer::factory(1000)->create();
-        \App\Models\Purchase::factory(100)->create();
+        Customer::factory(1000)->create();
+
+        $items = Item::all();
+
+        Purchase::factory(100)->create()
+        ->each(function(Purchase $purchase) use ($items) {
+           $purchase->items()->attach(
+            $items->random(rand(1,3))->pluck('id')->toArray(),
+            /* random(rand(1,3))は1-3のどれかではなく、1-3個摘出する */
+            /* > $items->random(rand(1,3))->pluck('id')->toArray();
+                = [ 
+                    1,
+                    3,
+                ] */
+            ['quantity' => rand(1,5)],
+           );
+        });
     }
 }
