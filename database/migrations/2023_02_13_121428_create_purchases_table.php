382546bef6842a39538fe163f02ff0ba35964c5e
yuta higa <h.yuta2314@gmail.com>
2023-02-13 13:05:53 +0900

    Migrated Purchase.

--- database/migrations/2023_02_13_121428_create_purchases_table.php
+++ database/migrations/2023_02_13_121428_create_purchases_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('purchases', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('customer_id')
+                  ->constrained()
+                  ->cascadeOnUpdate();
+            $table->boolean('status');
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         ]);
 
         \App\Models\Customer::factory(1000)->create();
+        \App\Models\Purchase::factory(100)->create();
     }
 }
